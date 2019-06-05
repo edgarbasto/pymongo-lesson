c79a5c13dc13ba1583af0047c2192f59835f2022
Edgar Basto <edgar.pt@gmail.com>
2019-06-05 18:08:39 +0100

    02

--- 01-consola.py
+++ 01-consola.py
@@ -17,7 +17,50 @@ pprint(serverStatusResult)
 client = pymongo.MongoClient('localhost', 27017)
 #pprint(dir(client))
 pprint(client.list_database_names())
-db = client.temp
-pprint(dir())
-pprint(db.command('serverStatus'))
+db = client.testar
+#pprint(dir(db))
+#pprint(db.command('serverStatus'))
 
+col = db.get_collection('qqcoisa')
+#pprint(dir(col))
+
+'''
+one_data = {
+    'titulo':'teste1',
+    'pessoas': ['Joao', 'Pedro', 'Manuela'],
+    'comida': {
+        'carne': 12,
+        'peixe': 2,
+        'morcelas': 4.2,
+    },
+    'localidade':'montalegre'
+}
+
+col.insert_one(one_data)
+'''
+'''
+two_data = [
+    {
+    'titulo':'teste2',
+    'pessoas': ['Maria', 'Tete', 'Toto'],
+    'comida': {
+        'carne': 2.5,
+        'peixe': 40,
+        'morcelas': 2.3,
+        },
+    'localidade':'montalegre'
+    }, 
+    {
+    'titulo':'teste3',
+    'pessoas': ['Isa', 'Ana Lee', 'Rui'],
+    'comida': {
+        'carne': 8,
+        'peixe': 23,
+        'morcelas': 15,
+        },
+    'localidade':'lisboa'
+    }
+]
+
+col.insert_many(two_data)
+'''
